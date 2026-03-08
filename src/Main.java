d50a55d587f134156223cf4eeb497b13567a6b9e
Judson Paiva <judson.paiva@hotmail.com>
2026-03-08 19:53:49 +0100

    o TaxService foi melhorado e aplicado O do SOLID e inversao de deppendencia

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 import entities.Employee;
+import service.GeralTaxService;
 import service.PensionService;
 import service.SalaryService;
 import service.TaxService;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 void main() {
 
     Employee employee = new Employee("Leonor", 4000);
 
     PensionService pensionService = new PensionService();
-    TaxService taxService = new TaxService();
+    GeralTaxService taxService = new TaxService();
 
     SalaryService salaryService = new SalaryService(pensionService, taxService);
 
     double netSalary = salaryService.netSalary(employee);
     IO.println("Net Salary: " + netSalary);
-
 }
